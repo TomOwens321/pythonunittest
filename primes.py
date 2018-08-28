1802e0bf9e5a478b7b24a99540f13ddcde08fc8b
Tom Owens <tom@towens.com>
2018-08-28 05:29:25 -0600

    Remove unused code

--- primes.py
+++ primes.py
@@ -14,12 +14,3 @@ def is_prime(number):
             return False
 
     return True
-
-def print_next_prime(number):
-    """print the closest prime number larger than *number*"""
-    index = number
-    while True:
-        index += 1
-        if is_prime(index):
-            print(index)
-
