ea6200a774f0d7bad31ef13cb050926f57e954a3
Tom Owens <tom@towens.com>
2018-08-29 01:28:50 +0000

    First mock test.

--- primes.py
+++ primes.py
@@ -14,3 +14,9 @@ def is_prime(number):
             return False
 
     return True
+
+def next_prime(number):
+    index = number
+    while not is_prime(index):
+        index += 1
+    print(index)
